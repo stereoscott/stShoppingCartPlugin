77af5466efef4afa7b30c143dba2e1d3adb93074
Scott Meves <scott@stereointeractive.com>
2011-06-22 14:45:38 -0400

    Merchant account, promo code updates

---
+++ lib/form/doctrine/PluginMerchantAccountForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * PluginMerchantAccount form.
+ *
+ * @package    ##PROJECT_NAME##
+ * @subpackage form
+ * @author     ##AUTHOR_NAME##
+ * @version    SVN: $Id: sfDoctrineFormPluginTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+abstract class PluginMerchantAccountForm extends BaseMerchantAccountForm
+{
+}
